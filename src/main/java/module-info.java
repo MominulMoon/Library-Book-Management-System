8ee0af2bada5d4101ef56f985084969227c433be
mehrinnasa <mommumu9@gmail.com>
2026-01-30 17:00:17 +0600

    1st try

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module mehrin.libraryloginpage {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens mehrin.libraryloginpage to javafx.fxml;
-    exports mehrin.libraryloginpage;
-}
