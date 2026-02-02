198c4ef1508422f7426f953929f824dece8b42e0
Mehrin Nasa Mom <mommumu9@gmail.com>
2026-02-02 17:48:27 +0600

    Delete src directory

--- src/main/java/mehrin/libraryloginpage/Launcher.java
+++
@@ -1,9 +0,0 @@
-package mehrin.libraryloginpage;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        MainApp.main(args);
-    }
-}
